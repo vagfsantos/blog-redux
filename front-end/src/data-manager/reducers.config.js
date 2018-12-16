4bfdb64f1978da6524892317c009e54f8673a2fb
Vagner Santos <vagfsantos@Vagners-MBP.mshome.net>
2018-12-16 21:25:08 -0200

    Add filters to store

--- front-end/src/data-manager/reducers.config.js
+++ front-end/src/data-manager/reducers.config.js
@@ -2,8 +2,10 @@ import { combineReducers } from "redux";
 
 import { categoryReducer as categories } from "../components/Menu/Menu.reducer";
 import { PostListReducer as posts } from "../components/PostList/PostList.reducer";
+import { FiltersReducer as filters } from "../components/Filters/Filters.reducer";
 
 export const reducerConfig = combineReducers({
   categories,
-  posts
+  posts,
+  filters
 });
