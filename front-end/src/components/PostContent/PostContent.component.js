d87f8176cee8649bbe4db9e233e7a29266263a75
Vagner Santos <vagfsantos@Vagners-MacBook-Pro.local>
2018-12-29 18:18:36 -0200

    Post not found

--- front-end/src/components/PostContent/PostContent.component.js
+++ front-end/src/components/PostContent/PostContent.component.js
@@ -6,35 +6,40 @@ import VoteContainer from "../Vote/Vote.container";
 import Score from "../Score/Score.component";
 import { VOTE_TYPE } from "../Vote/Vote.enum";
 
-const PostContent = ({ post = {} }) => (
-  <article className="box">
-    <header>
-      <h1 className="has-text-link title is-3">{post.title}</h1>
-      <PostMetaData
-        author={post.author}
-        timestamp={post.timestamp}
-        postId={post.id}
-      />
-      <Score voteScore={post.voteScore} commentCount={post.commentCount} />
-    </header>
+const PostContent = ({ post }) =>
+  post ? (
+    <article className="box">
+      <header>
+        <h1 className="has-text-link title is-3">{post.title}</h1>
+        <PostMetaData
+          author={post.author}
+          timestamp={post.timestamp}
+          postId={post.id}
+        />
+        <Score voteScore={post.voteScore} commentCount={post.commentCount} />
+      </header>
 
-    <main>
-      <p>{post.body}</p>
-    </main>
-
-    <br />
-
-    <footer>
-      <h3 className="title is-5">Let your vote</h3>
-      <VoteContainer id={post.id} type={VOTE_TYPE.POST} />
+      <main>
+        <p>{post.body}</p>
+      </main>
 
       <br />
 
-      <Link className="button is-dark" to={`/post/${post.id}/comment`}>
-        Comment
-      </Link>
-    </footer>
-  </article>
-);
+      <footer>
+        <h3 className="title is-5">Let your vote</h3>
+        <VoteContainer id={post.id} type={VOTE_TYPE.POST} />
+
+        <br />
+
+        <Link className="button is-dark" to={`/post/${post.id}/comment`}>
+          Comment
+        </Link>
+      </footer>
+    </article>
+  ) : (
+    <div className="box">
+      <h1 className="has-text-link title is-3">Post not found</h1>
+    </div>
+  );
 
 export default PostContent;
