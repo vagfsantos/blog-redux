756b6c8dd79b37b979d519adc8e630a451abd0eb
Vagner Santos <vagfsantos@Vagners-MacBook-Pro.local>
2019-01-12 22:43:45 -0200

    Fix tests

--- front-end/src/components/PostCard/PostContent.container.test.js
+++ front-end/src/components/PostCard/PostContent.container.test.js
@@ -3,18 +3,18 @@ import { mapStateToProps } from "./PostCard.container";
 describe("<PostCardContainer />", () => {
   it("returns the found post", () => {
     const posts = [{ id: 1 }, { id: 2 }, { id: 3 }];
-    const postId = 2;
+    const id = 2;
 
-    const props = mapStateToProps({ posts }, { postId });
+    const props = mapStateToProps({ posts }, { id });
 
-    expect(props.post.id).toBe(postId);
+    expect(props.post.id).toBe(id);
   });
 
   it("returns undefined when it cannot find the post ", () => {
     const posts = [{ id: 1 }, { id: 2 }, { id: 3 }];
-    const postId = 123456;
+    const id = 123456;
 
-    const props = mapStateToProps({ posts }, { postId });
+    const props = mapStateToProps({ posts }, { id });
 
     expect(props.post).toBeUndefined();
   });
